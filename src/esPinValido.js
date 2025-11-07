6893ddd1d41ade1e97d454ff97c77eeba4a3e266
PAULA ESCRIBANO REINA <pauescrei@alu.edu.gva.es>
2025-11-07 12:38:16 +0100

    refactor: simplificar la función esPinValido eliminando código redundante

--- src/esPinValido.js
+++ src/esPinValido.js
@@ -1,31 +1,25 @@
 function esPinValido(pin) {
-    if (typeof pin !== 'string') {
-    return false
-  }
+if (typeof pin !== 'string') return false
 
   const longitud = pin.length
-  if (longitud !== 4 && longitud !== 6) {
-    return false
-  }
+
+  if (longitud !== 4 && longitud !== 6) return false
 
   for (let i = 0; i < longitud; i++) {
-    const c = pin[i]
-    if (c < '0' || c > '9') {
-      return false
-    }
+    const caracter = pin[i]
+    if (caracter < '0' || caracter > '9') return false
   }
 
-  let todosIguales = true
+  const primerCaracter = pin[0]
+  let iguales = true
   for (let i = 1; i < longitud; i++) {
-    if (pin[i] !== pin[0]) {
-      todosIguales = false
+    if (pin[i] !== primerCaracter) {
+      iguales = false
       break
     }
   }
 
-  if (todosIguales) {
-    return false
-  }
+  if (iguales) return false
 
   return true
 }
