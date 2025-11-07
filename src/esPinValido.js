2ba521dda7c7179c5bea21f9a863be6705c06b87
PAULA ESCRIBANO REINA <pauescrei@alu.edu.gva.es>
2025-11-07 12:36:35 +0100

    implementacion de la funcion esPinValido para validar el formato del PIN

--- src/esPinValido.js
+++ src/esPinValido.js
@@ -1,7 +1,36 @@
 function esPinValido(pin) {
-  return false
+    if (typeof pin !== 'string') {
+    return false
+  }
+
+  const longitud = pin.length
+  if (longitud !== 4 && longitud !== 6) {
+    return false
+  }
+
+  for (let i = 0; i < longitud; i++) {
+    const c = pin[i]
+    if (c < '0' || c > '9') {
+      return false
+    }
+  }
+
+  let todosIguales = true
+  for (let i = 1; i < longitud; i++) {
+    if (pin[i] !== pin[0]) {
+      todosIguales = false
+      break
+    }
+  }
+
+  if (todosIguales) {
+    return false
+  }
+
+  return true
 }
 
 export {
     esPinValido
 }
+
